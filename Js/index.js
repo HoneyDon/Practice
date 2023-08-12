6472c165e6541ddec4f61bf8dee3177c63170069
HoneyDon <hj5799032@gmail.com>
2023-08-12 13:56:56 +0500

    Practice JS Basic

---
+++ Js/index.js
@@ -0,0 +1,123 @@
+let hassan = {
+ name : "Hassan",
+ age : "17",
+ grade : "A*",
+ height : "0",
+ location : "lahore"
+}
+
+let sara = {
+ name : "Sara",
+ age : "20",
+ grade : "",
+ height : "5.0",
+ location : "Islamabad"
+}
+
+let john = {
+ name : "John",
+ age : "0",
+ grade : "A*",
+ height : "6.1",
+ location : "Karachi"
+}
+
+let ahmad = {
+ name : "",
+ age : "35",
+ grade : "A*",
+ height : "5.2",
+ location : "0"
+}
+
+let umer = {
+ name : "Umer",
+ age : "",
+ grade : "A*",
+ height : "5.5",
+ location : ""
+}
+
+// HASSAN
+
+if (!!hassan.name) {
+    console.log("My name is " + hassan.name)
+}
+if (!!hassan.age) {
+    console.log("Hassan's age is " + hassan.age)
+}
+if (!!hassan.location) {
+    console.log("Hassan lives in " + hassan.location)
+}
+if (!!hassan.height) {
+    console.log("Hassan's height is " + hassan.height)   
+}
+if(!!hassan.grade) {
+    console.log("Hassan's grade is " + hassan.grade)
+}
+
+// SARA
+
+if (!!sara.name) {
+    console.log("My name is " + sara.name)
+}
+if (!!sara.age) {
+    console.log("Sara's age is " + sara.age)
+}
+if (!!sara.location) {
+    console.log("Sara lives in " + sara.location)
+}
+if (!!sara.height) {
+    console.log("Sara's height is " + sara.height)   
+}
+if (!!sara.grade) {
+    console.log("Sara's grade is " + sara.grade)
+}
+
+if (!!john.name) {
+    console.log("My name is " + john.name)
+}
+if (!!john.age) {
+    console.log("John's age is " + john.age)
+}
+if (!!john.location) {
+    console.log("John lives in " + john.location)
+}
+if (!!john.height) {
+    console.log("John's height is " + john.height)   
+}
+if (!!john.grade) {
+    console.log("John's grade is " + john.grade)
+}
+
+if (!!umer.name) {
+    console.log("My name is " + umer.name)
+}
+if (!!umer.age) {
+    console.log("Umer's age is " + umer.age)
+}
+if (!!umer.location) {
+    console.log("Umer lives in " + umer.location)
+}
+if (!!umer.height) {
+    console.log("Umer's height is " + umer.height)
+}
+if (!!umer.grade) {
+    console.log("Umer's grade is " + umer.grade)
+}
+
+if (!!ahmad.name) {
+    console.log("My name is " + ahmad.name)
+}
+if (!!ahmad.age) {
+    console.log("Ahmad's age is " + ahmad.age)
+}
+if (!!ahmad.location) {
+    console.log("Ahmad lives in " + ahmad.location)
+}
+if (!!ahmad.height) {
+    console.log("Ahmad's height is " + ahmad.height)
+}
+if (!!ahmad.grade) {
+    console.log("Ahmad's grade is " + ahmad.grade)
+}
