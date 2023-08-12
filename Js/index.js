4e25f31e5eadb09962f76cf02fdf6071d62a3e6e
HoneyDon <hj5799032@gmail.com>
2023-08-12 14:32:33 +0500

    Js Practice

--- Js/index.js
+++ Js/index.js
@@ -17,7 +17,7 @@ let sara = {
 let john = {
  name : "John",
  age : "0",
- grade : "A*",
+ grade : "A",
  height : "6.1",
  location : "Karachi"
 }
@@ -25,7 +25,7 @@ let john = {
 let ahmad = {
  name : "",
  age : "35",
- grade : "A*",
+ grade : "b*",
  height : "5.2",
  location : "0"
 }
@@ -40,84 +40,135 @@ let umer = {
 
 // HASSAN
 
-if (!!hassan.name) {
-    console.log("My name is " + hassan.name)
+console.log(!!hassan.name);
+if (hassan.name) {
+    console.log("My name is " + hassan.name);
 }
-if (!!hassan.age) {
-    console.log("Hassan's age is " + hassan.age)
+
+console.log(!!hassan.age);
+if (hassan.age) {
+    console.log("Hassan's age is " + hassan.age);
 }
-if (!!hassan.location) {
-    console.log("Hassan lives in " + hassan.location)
+
+console.log(!!hassan.height);
+if (hassan.height) {
+    console.log("Hassan's height is " + hassan.height);
 }
-if (!!hassan.height) {
-    console.log("Hassan's height is " + hassan.height)   
+
+console.log(!!hassan.grade);
+if (hassan.grade) {
+    console.log("Hassan's grade is " + hassan.grade);
 }
-if(!!hassan.grade) {
-    console.log("Hassan's grade is " + hassan.grade)
+
+console.log(!!hassan.location);
+if (hassan.location) {
+    console.log("Hassan lives in " + hassan.location);
 }
 
 // SARA
 
-if (!!sara.name) {
-    console.log("My name is " + sara.name)
-}
-if (!!sara.age) {
-    console.log("Sara's age is " + sara.age)
-}
-if (!!sara.location) {
-    console.log("Sara lives in " + sara.location)
-}
-if (!!sara.height) {
-    console.log("Sara's height is " + sara.height)   
-}
-if (!!sara.grade) {
-    console.log("Sara's grade is " + sara.grade)
+console.log(!!sara.name);
+if (sara.name) {
+    console.log("My name is " + sara.name);
 }
 
-if (!!john.name) {
-    console.log("My name is " + john.name)
-}
-if (!!john.age) {
-    console.log("John's age is " + john.age)
-}
-if (!!john.location) {
-    console.log("John lives in " + john.location)
-}
-if (!!john.height) {
-    console.log("John's height is " + john.height)   
-}
-if (!!john.grade) {
-    console.log("John's grade is " + john.grade)
+console.log(!!sara.age);
+if (sara.age) {
+    console.log("Sara's age is " + sara.age);
 }
 
-if (!!umer.name) {
-    console.log("My name is " + umer.name)
-}
-if (!!umer.age) {
-    console.log("Umer's age is " + umer.age)
-}
-if (!!umer.location) {
-    console.log("Umer lives in " + umer.location)
-}
-if (!!umer.height) {
-    console.log("Umer's height is " + umer.height)
-}
-if (!!umer.grade) {
-    console.log("Umer's grade is " + umer.grade)
+console.log(!!sara.height);
+if (sara.height) {
+    console.log("Sara's height is " + sara.height);
 }
 
-if (!!ahmad.name) {
-    console.log("My name is " + ahmad.name)
+console.log(!!sara.grade);
+if (sara.grade) {
+    console.log("Sara's grade is " + sara.grade);
 }
-if (!!ahmad.age) {
-    console.log("Ahmad's age is " + ahmad.age)
+
+console.log(!!sara.location);
+if (sara.location) {
+    console.log("Sara lives in " + sara.location);
 }
-if (!!ahmad.location) {
-    console.log("Ahmad lives in " + ahmad.location)
+
+// JOHN 
+
+console.log(!!john.name);
+if (john.name) {
+    console.log("My name is " + john.name);
 }
-if (!!ahmad.height) {
-    console.log("Ahmad's height is " + ahmad.height)
+
+console.log(!!john.age);
+if (john.age) {
+    console.log("John's age is " + john.age);
 }
-if (!!ahmad.grade) {
-    console.log("Ahmad's grade is " + ahmad.grade)
+
+console.log(!!john.height);
+if (john.height) {
+    console.log("John's height is " + john.height);
+}
+
+console.log(!!john.grade);
+if (john.grade) {
+    console.log("John's grade is " + john.grade);
+}
+
+console.log(!!john.location);
+if (john.location) {
+    console.log("John lives in " + john.location);
+}
+
+// AHMAD_
+
+console.log(!!ahmad.name);
+if (ahmad.name) {
+    console.log("My name is " + ahmad.name);
+}
+
+console.log(!!ahmad.age);
+if (ahmad.age) {
+    console.log("Ahmad's age is " + ahmad.age);
+}
+
+console.log(!!ahmad.height);
+if (ahmad.height) {
+    console.log("Ahmad's height is " + ahmad.height);
+}
+
+console.log(!!ahmad.grade);
+if (ahmad.grade) {
+    console.log("Ahmad's grade is " + ahmad.grade);
+}    
+
+console.log(!!ahmad.location);
+if (ahmad.location) {
+    console.log("Ahmad lives in " + ahmad.location);
+}
+
+// UMER
+
+console.log(!!umer.name);
+if (umer.name) {
+    console.log("My name is " + umer.name);
+}
+
+console.log(!!umer.age);
+if (umer.age) {
+    console.log("Umer's age is " + umer.age);
+}
+
+console.log(!!umer.height);
+if (umer.height) {
+    console.log("Umer's height is " + umer.height);
+}
+
+console.log(!!umer.grade);
+if (umer.grade) {
+    console.log("Umer's grade is " + umer.grade);
+}
+
+console.log(!!umer.location);
+if (umer.location) {
+    console.log("Umer lives in " + umer.location);
 }
