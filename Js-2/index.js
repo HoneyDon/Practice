0f13ff94551c4f5aa226155973c9a86b4d513fdc
HoneyDon <hj5799032@gmail.com>
2023-08-13 17:05:29 +0500

    Basic practice of functions

---
+++ Js-2/index.js
@@ -0,0 +1,66 @@
+// ADDITION__
+
+const addthreenumbers = (num1, num2, num3) => {
+    try {
+        return (num1 + num2 + num3);
+    } catch (error) {
+        return error.messagge
+    }
+}
+
+console.log(addthreenumbers(44, 23, 7));
+
+// SUBTRACTION__
+
+const subtractthreenumbers = (num1, num2, num3) => {
+    try {
+        return (num1 - num2 - num3);
+    } catch (error) {
+        return error.messagge
+    }
+}
+
+console.log(subtractthreenumbers(40, 22, 1));
+
+// MULTIPLICATION__
+
+const multiplythreenumbers = (num1, num2, num3) => {
+    try {
+        return (num1 * num2 * num3);
+    } catch (error) {
+        return error.messagge
+    }
+}
+
+console.log(multiplythreenumbers(11, 6, 2));
+
+// DIVISION__
+
+const dividethreenumbers = (num1, num2, num3) => {
+    try {
+        return (num1 / num2 / num3);
+    } catch (error) {
+        return error.messagge
+    }
+}
+
+console.log(dividethreenumbers(1000,50,5));
+
+// ODD OR EVEN__
+
+const oddoreven = (num1) => {
+    try {
+        if (num1 % 2 === 0) {
+            return `${num1} is even`
+        } else {
+            return `${num1} is odd`
+        }
+    } catch (error) {
+        return error.messagge
+    }
+}
+
+console.log(oddoreven(80));
+console.log(oddoreven(21));
+
+
