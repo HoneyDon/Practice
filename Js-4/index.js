d18a78aa03adf26aa0e5ee3cf093606ccb164c44
HoneyDon <hj5799032@gmail.com>
2023-09-09 20:53:39 +0500

    Created a List using InnerHtml

--- Js-4/index.js
+++ Js-4/index.js
@@ -7,3 +7,12 @@ const changecolor = () => {
         console.log(error.message)
     }
 }
+
+const createlist = () => {
+    try {
+        const getHTMLElement = document.querySelector(".inner-list")
+        getHTMLElement.innerHTML += "<li>Item 1</li>"
+    } catch (error) {
+        console.log(error.message)
+    }
+}
