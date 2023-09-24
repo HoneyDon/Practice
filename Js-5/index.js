f31dd9021ddbb3beed2b163983d0cd72bc8bc8ad
HoneyDon <hj5799032@gmail.com>
2023-09-24 17:56:35 +0500

    Calculator Done!

--- Js-5/index.js
+++ Js-5/index.js
@@ -1,4 +1,28 @@
-function calculate() {
-    var result = document.getElementById("result");
-    result.value = eval(result.value);
-}
+// index.js
+
+// Get the input and result elements
+var result = document.getElementById("result");
+var buttons = document.querySelectorAll("#calcu input[type='button']");
+
+// Add click event listeners to the buttons
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        var value = button.value;
+
+        if (value === "=") {
+            try {
+                // Evaluate the expression and update the result
+                result.value = eval(result.value);
+            } catch (error) {
+                // Handle error and display "Error" in case of invalid expression
+                result.value = "Error";
+            }
+        } else if (value === "c") {
+            // Clear the input field
+            result.value = "";
+        } else {
+            // Append the button value to the input field
+            result.value += value;
+        }
+    });
+});
